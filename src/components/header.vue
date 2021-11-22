f8698737291546015298ddb88e4febdac5320ef0
artemisia000 <misiapierobon@gmail.com>
2021-11-22 16:45:11 +0100

    first

---
+++ src/components/header.vue
@@ -0,0 +1,20 @@
+<template>
+
+<header>
+    <h1>“La mia prima app con Vue CLI!”</h1>
+    <img src="./assets/logo.png" alt="">
+</header>
+
+  
+</template>
+
+<script>
+export default {
+    name: 'header'
+
+}
+</script>
+
+<style>
+
+</style>
